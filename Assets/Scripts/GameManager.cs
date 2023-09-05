065fce1e02a3fab40b9c0b83b4950a5b6113ee5b
pedrogaldo <101400428+pedrogaldo@users.noreply.github.com>
2023-09-05 20:04:47 -0300

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public SpriteRenderer tamborRojo;
+    public SpriteRenderer tamborAzul;
+    public SpriteRenderer tamborVerde;
+
+    Color newColor = new Color(0.3f, 0.4f, 0.6f, 0.3f);
+    public Color m_newColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            //tamborRojo.GetComponent<AudioSource>().enabled = true;
+            tamborRojo.color = Color.red;
+
+        }
+
+        else
+        {
+           // GetComponent<AudioSource>().enabled = false;
+            tamborRojo = GetComponent<SpriteRenderer>();
+            tamborRojo.color = Color.grey;
+        }
+    }
+}
